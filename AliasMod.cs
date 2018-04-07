1e9fadd1912fa076498e957aaae821bc8193ed7a
Alana Gilston <alana@abluescarab.us>
2018-04-06 19:09:44 -0700

    v4.6

--- AliasMod.cs
+++ AliasMod.cs
@@ -7,7 +7,7 @@ using Pathfinder.Util;
 namespace AliasMod {
     public class AliasMod : IMod {
         public const string Name = "Alias Mod";
-        public const string Version = "4_5";
+        public const string Version = "4_6";
         public static KeyValueFile File;
         public const string ID = Name + " v" + Version;
         public const string Homepage = "https://github.com/abluescarab/hacknet-aliasmod";
